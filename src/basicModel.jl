266d6f274a6553caa53d238b6da44f7432cb63bb
Gregory Roth <greg.roth51283@gmail.com>
2024-11-05 15:52:08 +0100

    test on 2s3r model

--- src/basicModel.jl
+++ src/basicModel.jl
@@ -6,7 +6,8 @@ $(TYPEDEF)
 struct StandardStoModel
     nbstate::Int
     nbparameters::Int
-    ParamToRate::Array{Float64,2}
+    ParamToRate_idx::Vector{CartesianIndex{2}}
+    ParamToRate_val::Vector{Int}
     TrState::Vector{Int}
 end
 
@@ -20,18 +21,26 @@ Define a model instance based on the structured of the model,
 the parameters (transition rate between states), and the initiation and degrradation rate
 """
 function StoModel(model::StandardStoModel, parameters::Vector{Float64},kini::Float64,delta::Float64, maxrna::Int)
-    Qstate = zeros(model.nbstate) 
-    Q = zeros(model.nbstate*(maxrna+1))
-    for (idx,val) in enumerate(model.ParamToRate)
-        Qstate[idx] = parameters[val]
+    Qstate = zeros(model.nbstate,model.nbstate) 
+    Q = zeros(model.nbstate*(maxrna+1),model.nbstate*(maxrna+1))
+    for i in eachindex(model.ParamToRate_val)
+        Qstate[model.ParamToRate_idx[i]] = parameters[model.ParamToRate_val[i]]
     end
     for i=0:maxrna
         Q[i*model.nbstate+1:(i+1)*model.nbstate,i*model.nbstate+1:(i+1)*model.nbstate] .= Qstate
     end
-    for i=1:model.nbstate*(maxrna)
-        Q[i,model.nbstate+i] = kini
-        Q[model.nbstate+i,i] = delta
-    end 
+
+    for i=0:maxrna-1
+        for j in eachindex(model.TrState)
+            Q[i*model.nbstate+model.TrState[j],i*model.nbstate+model.TrState[j]+model.nbstate] = kini
+        end
+        for j = 1:model.nbstate
+            Q[(i+1)*model.nbstate+j,i*model.nbstate+j] = delta*(i+1)
+        end
+    end
+    for i in axes(Q,1)
+        Q[i,i] = Q[i,i] -sum(Q[i,:])
+    end
     
     return exp(Q)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,32 @@ using StoThyLiveCell
 using Test
 
 
+#define the 2s 3r model
+#(r12,r21,r23,r32,k1on,k2on,k3on,koff)
+Qstate = [0    8    4    0    0    0;
+          7    0    0    4    0    0;
+          3    0    0    8    2    0;
+          0    3    6    0    0    2;
+          0    0    1    0    0    8;
+          0    0    0    1    5    0]
+paramToRate_idx = findall(Qstate .>0)
+paramToRate_val = Qstate[findall(Qstate .>0)]
+model1 = StoThyLiveCell.StandardStoModel(6,8,paramToRate_idx,paramToRate_val,[1,3,5])
+
+#creating an instance 
+parameters = [0.0178504,  0.0436684,  0.0543096,  0.427785,  0.023986,  0.308174,  2.24418,  1.28387]
+kini = 3.80846
+delta = 1.
+maxrna = 25
+
+P1 = StoModel(model1, parameters,kini,delta, maxrna)
+
+timevec = 1:1:200
+timevec_on = 1:1:10
+timevec_int = 1:1:20
+
+(mnascentmrna_model, pburst_model, survivalspot_model,survivaldark_model, survivalnextburst_model, corr_interburst_model, intensity_model) =  ModelOutput(model1, parameters,kini,delta, maxrna,timevec_on[end],timevec[end],timevec[end],timevec_int[end])
+
 
 #= @testset "StoThyLiveCell.jl" begin
     # Write your tests here.
