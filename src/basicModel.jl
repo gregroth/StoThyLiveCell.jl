84f5893485462e8dfae0183ac6a71a1ec3fa49b4
Gregory Roth <greg.roth51283@gmail.com>
2024-11-07 10:51:40 +0100

    add StoModel with Q as argument

--- src/basicModel.jl
+++ src/basicModel.jl
@@ -45,5 +45,34 @@ function StoModel(model::StandardStoModel, parameters::Vector{Float64},kini::Flo
     return exp(Q)
 end
 
+"""
+    StoModel!(Q::Array{Float64,2}, model::StandardStoModel, parameters::Vector{Float64},kini::Float64,delta::Float64, maxrna::Int)
+
+ Modify Q in P corresponding to the transition matrix for a model instance based on the structured of the model, 
+    the parameters (transition rate between states), and the initiation and degrradation rate
+"""
+function StoModel!(Q::Array{Float64,2}, model::StandardStoModel, parameters::Vector{Float64},kini::Float64,delta::Float64, maxrna::Int)
+    Qstate = zeros(model.nbstate,model.nbstate) 
+    for i in eachindex(model.ParamToRate_val)
+        Qstate[model.ParamToRate_idx[i]] = parameters[model.ParamToRate_val[i]]
+    end
+    for i=0:maxrna
+        Q[i*model.nbstate+1:(i+1)*model.nbstate,i*model.nbstate+1:(i+1)*model.nbstate] .= Qstate
+    end
+
+    for i=0:maxrna-1
+        for j in eachindex(model.TrState)
+            Q[i*model.nbstate+model.TrState[j],i*model.nbstate+model.TrState[j]+model.nbstate] = kini
+        end
+        for j = 1:model.nbstate
+            Q[(i+1)*model.nbstate+j,i*model.nbstate+j] = delta*(i+1)
+        end
+    end
+    for i in axes(Q,1)
+        Q[i,i] = Q[i,i] -sum(Q[i,:])
+    end
+    Q .= exp(Q)
+end
+
 
 
