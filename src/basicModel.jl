26d5ddaa50dc9d429dbe63ee676a20d632360179
Gregory Roth <greg.roth51283@gmail.com>
2024-11-08 13:35:50 +0100

    add basic fct to return rate matrix

--- src/basicModel.jl
+++ src/basicModel.jl
@@ -94,3 +94,38 @@ end
 
 
 
+"""
+    StoModel_RateMat(model::StandardStoModel, parameters::Vector{Float64},kini::Vector{Float64},delta::Float64, maxrna::Int)
+
+Return the rate matrix of a model instance based on the topology of the model.
+
+#Arguments
+- `parameters::Vector{Float64}`: list of the rates of the transition between the 'promoter' states
+- `kini::Vector{Float64}`: list of the initiation rates corresponding the list of active 'promoter' states described in model.TrState 
+- `delta::Float64`: degradation/release rate
+- `maxrna::Int`: maximum number of mRNA considered in the model
+"""
+function StoModel_RateMat(model::StandardStoModel, parameters::Vector{Float64},kini::Vector{Float64},delta::Float64, maxrna::Int)
+    Qstate = zeros(model.nbstate,model.nbstate) 
+    Q = zeros(model.nbstate*(maxrna+1),model.nbstate*(maxrna+1))
+    for i in eachindex(model.ParamToRate_val)
+        Qstate[model.ParamToRate_idx[i]] = parameters[model.ParamToRate_val[i]]
+    end
+    for i=0:maxrna
+        Q[i*model.nbstate+1:(i+1)*model.nbstate,i*model.nbstate+1:(i+1)*model.nbstate] .= Qstate
+    end
+
+    for i=0:maxrna-1
+        for j in eachindex(model.TrState)
+            Q[i*model.nbstate+model.TrState[j],i*model.nbstate+model.TrState[j]+model.nbstate] = kini[j]
+        end
+        for j = 1:model.nbstate
+            Q[(i+1)*model.nbstate+j,i*model.nbstate+j] = delta*(i+1)
+        end
+    end
+    for i in axes(Q,1)
+        Q[i,i] = Q[i,i] -sum(Q[i,:])
+    end
+    
+    return Q
+end
