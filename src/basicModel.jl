5a07f0ca4a1ef3898c082d7fe8fad990c5133539
Gregory Roth <greg.roth51283@gmail.com>
2024-11-08 08:35:44 +0100

    add documentatio for the basic model fct and struct

--- src/basicModel.jl
+++ src/basicModel.jl
@@ -2,12 +2,18 @@
 
 """
 $(TYPEDEF)
+$(TYPEDFIELDS)
 """
 struct StandardStoModel
+    "number of 'promoter' states"
     nbstate::Int
+    "number of parameter, including initiation and degradation"
     nbparameters::Int
+    "list of the cartesian indices for which the rate matrix is non zero"
     ParamToRate_idx::Vector{CartesianIndex{2}}
+    "list of the parameter indices corresponding to the ParamToRate_idx entries of the rate matrix"
     ParamToRate_val::Vector{Int}
+    "list of the 'promoter' states from which the initiation rate is non zero"
     TrState::Vector{Int}
 end
 
@@ -15,12 +21,16 @@ end
 
 
 """
-    StoModel(model::StandardStoModel, parameters::Vector{Float64},kini::Float64,delta::Float64)
+    StoModel(model::StandardStoModel, parameters::Vector{Float64},kini::Vector{Float64},delta::Float64)
 
-Define a model instance based on the structured of the model, 
-the parameters (transition rate between states), and the initiation and degrradation rate
+Define a model instance based on the topology of the model.
+    
+#Arguments
+- `parameters::Vector{Float64}`: list of the rates of the transition between the 'promoter' states
+- `kini::Vector{Float64}`: list of the initiation rates corresponding the list of active 'promoter' states described in model.TrState 
+- `delta::Float64`: degradation/release rate
 """
-function StoModel(model::StandardStoModel, parameters::Vector{Float64},kini::Float64,delta::Float64, maxrna::Int)
+function StoModel(model::StandardStoModel, parameters::Vector{Float64},kini::Vector{Float64},delta::Float64, maxrna::Int)
     Qstate = zeros(model.nbstate,model.nbstate) 
     Q = zeros(model.nbstate*(maxrna+1),model.nbstate*(maxrna+1))
     for i in eachindex(model.ParamToRate_val)
