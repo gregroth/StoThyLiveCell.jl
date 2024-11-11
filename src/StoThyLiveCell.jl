6248d7b844e21236737d8e9d6662be1bb00d2682
Gregory Roth <greg.roth51283@gmail.com>
2024-11-11 13:32:56 +0100

    export plotALl

--- src/StoThyLiveCell.jl
+++ src/StoThyLiveCell.jl
@@ -9,6 +9,7 @@ include("basicModel.jl")
 include("basicAnalysis.jl")
 
 export StandardStoModel, StoModel, ModelOutput
-export mo_basics, mo_mnascent, mo_ontime, mo_offtime, mo_nextbursttime, mo_pon, mo_interburstcorr, mo_avgintensity, plotAll
+export mo_basics, mo_mnascent, mo_ontime, mo_offtime, mo_nextbursttime, mo_pon, mo_interburstcorr, mo_avgintensity
+export plotAll
 
 end
