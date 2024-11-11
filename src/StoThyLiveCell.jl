280eb6de589166230e5908b58dee051684b5b1f4
Gregory Roth <greg.roth51283@gmail.com>
2024-11-11 13:35:38 +0100

    include basicPlot

--- src/StoThyLiveCell.jl
+++ src/StoThyLiveCell.jl
@@ -7,6 +7,7 @@ using Plotly
 
 include("basicModel.jl")
 include("basicAnalysis.jl")
+include("basicPlot.jl")
 
 export StandardStoModel, StoModel, ModelOutput
 export mo_basics, mo_mnascent, mo_ontime, mo_offtime, mo_nextbursttime, mo_pon, mo_interburstcorr, mo_avgintensity
