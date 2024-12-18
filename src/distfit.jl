81a3e8a3a046bda828d7ac050ae22a8496392f23
Gregory Roth <greg.roth51283@gmail.com>
2024-12-18 13:44:03 +0100

    add weight in dist fct and struct

--- src/distfit.jl
+++ src/distfit.jl
@@ -44,23 +44,23 @@ end
 function (f::LikelihoodRNA)(estimate_signal::Vector, ref_signal::Vector; kwargs...)
     estimate_signal_ = @. max(estimate_signal, 0)
     ind = @. Int(floor(ref_signal) + 1) # because the index is from 0 
-    -sum(log.(estimate_signal_[ind] .+ 1e-6)) # add a small quantity to avoid log(0)
+    -sum(log.(estimate_signal_[ind] .+ 1e-6))*f.weight # add a small quantity to avoid log(0)
 end
 
 function (f::LsqSurvival)(estimate_signal::Vector, ref_signal::Tuple{Vector,Vector}; kwargs...)
-    sum((log.(estimate_signal) - log.(ref_signal[2])).^2)/length(estimate_signal)
+    f.weight * sum((log.(estimate_signal) - log.(ref_signal[2])).^2)/length(estimate_signal)
 end
 
 function (f::LsqSurvivalLogLin)(estimate_signal::Vector, ref_signal::Tuple{Vector,Vector}; kwargs...)
-    (f.plog * sum((log.(estimate_signal) - log.(ref_signal[2])).^2) + (1-f.plog) * sum(((estimate_signal) - (ref_signal[2])).^2) )/length(estimate_signal)
+    f.weight * (f.plog * sum((log.(estimate_signal) - log.(ref_signal[2])).^2) + (1-f.plog) * sum(((estimate_signal) - (ref_signal[2])).^2) )/length(estimate_signal)
 end
 
 function (f::LsqProb)(estimate_signal_tot::Float64, ref_signal::Float64; kwargs...)
-    (log.(estimate_signal_tot) - log.(ref_signal)).^2
+    f.weight * (log.(estimate_signal_tot) - log.(ref_signal)).^2
 end
 
 function (f::LsqNumber)(estimate_signal_tot::Float64, ref_signal::Float64; kwargs...)
-    (estimate_signal_tot - ref_signal)^2
+    f.weight * (estimate_signal_tot - ref_signal)^2
 end
 
 
