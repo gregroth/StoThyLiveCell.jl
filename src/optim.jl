7e1cd84de0a2c78da0fd74573a25d155de207703
Gregory Roth <greg.roth51283@gmail.com>
2025-03-27 13:50:25 +0100

    add convoluted distribution to the ini_data function

--- src/optim.jl
+++ src/optim.jl
@@ -339,7 +339,7 @@ function ini_data(optim_struct::OptimStruct, FRange; kwargs...)
     @unpack data = optim_struct
     datafit = []
     for i in eachindex(FRange)
-        if (FRange[i][2]>0) & !(data.datatypes[i] == StoThyLiveCell.Distribution_RNA())
+        if (FRange[i][2]>0) & !(data.datatypes[i] == StoThyLiveCell.Distribution_RNA()) & !(data.datatypes[i] == StoThyLiveCell.ConvoolutedDistribution_RNA())
             llimit = findfirst(data.data[i][1] .>=FRange[i][1])
             ulimit = findlast(data.data[i][1] .<=FRange[i][2])
             datatemp = (data.data[i][1][llimit:ulimit], data.data[i][2][llimit:ulimit],) 
@@ -347,6 +347,8 @@ function ini_data(optim_struct::OptimStruct, FRange; kwargs...)
                 @warn "There are survival probabilities equal or below zero"
             end
         elseif data.datatypes[i] == StoThyLiveCell.Distribution_RNA()
+            datatemp = data.data[i][(data.data[i] .>=FRange[i][1]) .& (data.data[i] .<=FRange[i][2])]
+        elseif data.datatypes[i] == StoThyLiveCell.ConvolutedDistribution_RNA()
             datatemp = data.data[i][(data.data[i] .>=FRange[i][1]) .& (data.data[i] .<=FRange[i][2])]
         else
             datatemp = data.data[i]
